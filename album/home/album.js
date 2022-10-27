b40534ad7d952a61cb54e95266a564c6004ce712
Agung Hidayat <agung.grafic147@gmail.com>
2022-10-27 18:47:48 +0700

    4ed4

--- album/home/album.js
+++ album/home/album.js
@@ -4,12 +4,6 @@ fetch("/album/home/album.html").then((snap) => snap.text()).then((result) => {
 
     // Posisi Album
     $(".posisi_album").addClass("col-xl-3 col-lg-3 col-md-3 col-sm-3 col-3 text-center")
-    $(".posisi_episode").addClass("position-absolute top-0 start-0 bg-black bg-opacity-75 text-light ps-1 pe-1")
+    $(".posisi_skin").addClass("position-absolute top-0 start-0 bg-black bg-opacity-75 text-light ps-1 pe-1")
     $(".posisi_judul").addClass("position-absolute bottom-0 start-0 end-0 ps-1 pe-1")
 });
-
-
-// // Home Completed 8 Album
-// for (let i = 1; i <= 20; i++) {
-//     $("#miya_" + i).removeClass("d-none");
-// }
